2f868047194a7e2a988f623b18533cf976b8a065
Camarederic <camarederic@mail.ru>
2022-11-12 20:01:59 +0300

    Create NavController

--- app/src/main/java/com/example/navigationcomponent_49/SecondFragment.kt
+++ app/src/main/java/com/example/navigationcomponent_49/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_second.*
 
 
 class SecondFragment : Fragment() {
@@ -18,5 +19,16 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        button_second_next.setOnClickListener {
+            (activity as MainActivity).navController.navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+
+        button_second_back.setOnClickListener {
+            (activity as MainActivity).navController.navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
 
 }
